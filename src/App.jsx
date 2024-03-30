7196d19857768608cc8b7517319df1ee56c559d3
Jaime Sebastián <jaiminator21@gmail.com>
2024-03-30 18:19:36 +0100

    css cartas y paginas añadidas al app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import './App.css'
-import Country from './layout/Country'
+import Header from './layout/Header'
+import Landing from './pages/Landing'
+import Countries from './pages/Countries'
 
 
 function App() {
@@ -10,7 +13,15 @@ function App() {
 
   return (
     <>
-      <Country/>
+    <Router> 
+      <Header/>
+      <main>
+        <Routes>
+        <Route path="" element ={<Landing/>}/>
+          <Route path="countries" element ={<Countries/>}/>
+        </Routes>  
+      </main>
+    </Router> 
     </>
   )
 }
---
+++ src/pages/Countries.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Country from '../layout/Country'
+
+const Countries = () => {
+  return (
+    <>
+    <Country/>
+    </>
+  )
+}
+
+export default Countries
