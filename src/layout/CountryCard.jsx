9c341ef1573db495b9b98771078ca1d24dba8ba8
Jaime Sebastián <jaiminator21@gmail.com>
2024-03-30 14:46:50 +0100

    fetch de paises + cartas de los paises

--- src/layout/CountryCard.jsx
+++ src/layout/CountryCard.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const CountryCard = () => {
+const CountryCard = (props) => {
+    const [language, setLanguage] = useState([])
+    const data = props.data
+    //console.log(data.lang);
+    const coin = data.coin
   return (
-    <div>CountryCard</div>
+   <>
+        <div className='container-card'>
+            <div className='container-card-upper'>
+             <img src={data.flag.svg} alt={data.flag.alt} /> 
+                
+            </div>
+            <div className='container-card-lower'>
+                <div className='container-country-stat'>
+                    <h1>{data.name.official}</h1>
+                </div>
+                <div className='container-country-stat'>
+                   <h1>{data.lang}</h1>
+                </div>
+                <div className='container-country-stat'>
+                   <h1>{data.coin}</h1>
+                </div>
+                <div className='container-country-stat'>
+                   <h1>{data.cont}</h1>
+                </div>
+            </div>
+        </div>
+   </>
   )
 }
 
