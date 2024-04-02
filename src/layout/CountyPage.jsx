a268f5cfb7ea10fbc6cd66c9f5ff28666a8627f7
jaiminator21 <jaiminator21@gmail.com>
2024-04-02 20:07:10 +0200

    Acceso a dato nativename official

--- src/layout/CountyPage.jsx
+++ src/layout/CountyPage.jsx
@@ -7,14 +7,18 @@ const CountyPage = (props) => {
 
     let listNative = []
     for (const key in native) {
+       // console.log(native[key].official);
         listNative.push(key)
     }
-    console.log(data.nameNative[`${listNative[0]}`]);
-    console.log(data.nameNative[`${listNative[0].common}`]);
+    ///console.log("Array de valores del objeto", listNative);
+    /* console.log("Sin entrar en el objeto", data.nameNative);
+    console.log("Sin especificar",data.nameNative[`${listNative[0]}`]);*/
+    console.log("Especificando oficial",data.nameNative[listNative[0]].official); 
   return (
     <>
     <div className='page-title'>
         <h1>{data.name.official}</h1>
+        <h2>{data.nameNative[listNative[0]].official}</h2>
         
      
     </div>
